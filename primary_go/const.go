fe47421770fe4387e2646447389ea8344f530e77
nickle <2330458484@qq.com>
2019-05-28 18:13:33 +0800

    Create const.go

---
+++ primary_go/const.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name = "pynickle"
+	const sex string = "man"
+	const(
+		a=iota
+		b=iota
+	)
+	fmt.Println(a,b)
+	const(
+		c=iota
+		d="string"
+		e
+		f=iota
+	)
+	fmt.Println(c,d,e,f)
+}
